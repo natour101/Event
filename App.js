c4739d0639fc7374790c7915d8381413c4aff090
natour101 <97555884+natour101@users.noreply.github.com>
2026-01-22 18:44:05 +0300

    .

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
 import { LanguageProvider } from './src/context/LanguageContext';
 import { ThemeProvider } from './src/context/ThemeContext';
-import AppNavigator from './navigation/AppNavigator';
 
 export default function App() {
   return (
